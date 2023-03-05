4a01305b67e0852d3d731bab28e4b8550da2c999
Agym Mahaputra Romy Barlianta <107300588+agymjz09@users.noreply.github.com>
2023-03-05 19:06:13 +0800

    Add files via upload

---
+++ TUGAS_3/Differences_IMG/difference_img.py
@@ -0,0 +1,19 @@
+#Nama : Agym Mahaputra Romy Barlianta
+#NIM  : F55121076
+import cv2
+import numpy as np
+
+pic1 = cv2.imread('11.jpg')
+pic2 = cv2.imread('12.jpg')
+
+gray_pic1 = cv2.cvtColor(pic1, cv2.COLOR_BGR2GRAY)
+gray_pic2 = cv2.cvtColor(pic2, cv2.COLOR_BGR2GRAY)
+
+diff = cv2.absdiff(gray_pic1, gray_pic2)
+
+eq_diff_pic = cv2.equalizeHist(diff)
+
+cv2.imshow('Difference Picture', diff)
+cv2.imshow('Histogram Equalization Difference Image', eq_diff_pic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
